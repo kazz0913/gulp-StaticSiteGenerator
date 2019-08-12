365271ecd27b438b88cf4361fb194dd1ea1a91a9
kazuki <sbr0913@gmail.com>
2019-08-12 21:46:49 +0900

    Add imagemin  #8

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,30 @@ const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 const ejs = require('gulp-ejs');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const browserSync = require('browser-sync').create();
 
 const webpackConfig = require('./webpack.config');
+
+const imageminOptions = [
+  pngquant(
+    {
+      quality: [0.65, 0.8],
+      speed: 1
+    }
+  ),
+  mozjpeg({
+    quality: 60
+  }),
+  imagemin.gifsicle(),
+  imagemin.jpegtran(),
+  imagemin.optipng(),
+  imagemin.svgo()
+]
 
 const browserSyncOption = {
   server: './dist'
@@ -29,6 +48,12 @@ gulp.task('ejs', () => {
     )
     .pipe(ejs({ msg: 'Hello Gulp!' }, {}, { ext: '.html' }))
     .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('imagemin', () => {
+  return gulp.src('./src/img/**/*.{jpg,png,gif,svg}')
+  .pipe(imagemin(imageminOptions))
+  .pipe(gulp.dest('./dist/assets/img'))
 })
 
 gulp.task('bundle', () => {
